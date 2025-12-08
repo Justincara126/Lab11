19491fdc8a011efb9ebe84bd50b0079dc58db6f9
Justincara126 <okonerolaf@gmail.com>
2025-12-08 15:17:24 +0100

    terminè.

--- model/rifugio.py
+++ model/rifugio.py
@@ -3,11 +3,16 @@ from dataclasses import dataclass
 from model.connessione import Connessione
 
 
+
+
 @dataclass
 class Rifugio:
     id: int
     nome: str
     localita: str
     altitude: int
-    capienza:int
     aperto: bool
+    def __str__(self):
+        return f'{self.id}  {self.nome}'
+    def __hash__(self):
+        return hash(self.id)
